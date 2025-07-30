84791b3ad0c1c93ce440050da24506fffa9720b7
HelloX3030 <120511705+HelloX3030@users.noreply.github.com>
2025-07-30 16:50:51 +0200

    added tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,16 +1,22 @@
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include <iostream>
 
 int main(void)
 {
-	ClapTrap bot1 = ClapTrap("Bot1");
+	ScavTrap bot1 = ScavTrap("Bot1");
 	bot1.attack("Dummy1");
 	bot1.takeDamage(20);
 	bot1.beRepaired(14);
 	bot1.attack("Dummy1");
-	ClapTrap bot2 = ClapTrap("Bot2");
+	ScavTrap bot2 = ScavTrap("Bot2");
 	bot2.beRepaired(5);
 	bot2.takeDamage(3);
 	for (int i = 0; i < 12; i++)
 		bot2.attack("Dummy2");
 	bot2.beRepaired(10);
+	std::cout << "========================================================================" << std::endl;
+	ScavTrap bot3 = ScavTrap("Bot3");
+	bot3.guardGate();
+	bot3 = bot2;
+	bot3.attack("Dummy3");
 }
